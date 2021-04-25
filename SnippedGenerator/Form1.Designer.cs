f27096cf7f9eaae5eb2c25c48b930fbab4356630
Fabian <fabian.fried04@gmail.com>
2021-04-25 22:47:42 +0200

    Add project files.

---
+++ SnippedGenerator/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace SnippedGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_Shortcut = new System.Windows.Forms.TextBox();
+            this.textBox_Description = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(41, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shortcut";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(41, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Shortcut Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(41, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Code";
+            // 
+            // textBox_Shortcut
+            // 
+            this.textBox_Shortcut.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_Shortcut.Location = new System.Drawing.Point(41, 56);
+            this.textBox_Shortcut.Name = "textBox_Shortcut";
+            this.textBox_Shortcut.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox_Shortcut.Size = new System.Drawing.Size(147, 29);
+            this.textBox_Shortcut.TabIndex = 3;
+            // 
+            // textBox_Description
+            // 
+            this.textBox_Description.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_Description.Location = new System.Drawing.Point(41, 144);
+            this.textBox_Description.Multiline = true;
+            this.textBox_Description.Name = "textBox_Description";
+            this.textBox_Description.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Description.Size = new System.Drawing.Size(369, 77);
+            this.textBox_Description.TabIndex = 4;
+            this.textBox_Description.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(41, 272);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(740, 299);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 694);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox_Description);
+            this.Controls.Add(this.textBox_Shortcut);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_Shortcut;
+        private System.Windows.Forms.TextBox textBox_Description;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SnippedGenerator/ReadMe.txt
@@ -0,0 +1,18 @@
+﻿FF - 25-04-2021
+=================
+Snippet generator
+
+
+
+Input:	Shortcut
+		Shortcut Description	
+		Code
+		Extras
+
+Output: a .snipped file
+
+
+The Snipped Template is in Snippet_template.txt
+to replace are all *number* (*1*, *2*, ...)
+
+
