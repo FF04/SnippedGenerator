a1ca72fccbfdeafab135c29ff7d91ff75931338f
Fabian <fabian.fried04@gmail.com>
2021-04-27 15:37:44 +0200

    speicher

--- SnippedGenerator/Form1.Designer.cs
+++ SnippedGenerator/Form1.Designer.cs
@@ -35,6 +35,13 @@ namespace SnippedGenerator
             this.textBox_Shortcut = new System.Windows.Forms.TextBox();
             this.textBox_Description = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AddType_Button = new System.Windows.Forms.Button();
+            this.comboBox_types = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_typeToolTip = new System.Windows.Forms.TextBox();
+            this.textBox_defaultValue = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -72,6 +79,7 @@ namespace SnippedGenerator
             this.textBox_Shortcut.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.textBox_Shortcut.Location = new System.Drawing.Point(41, 56);
             this.textBox_Shortcut.Name = "textBox_Shortcut";
+            this.textBox_Shortcut.PlaceholderText = "cr";
             this.textBox_Shortcut.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
             this.textBox_Shortcut.Size = new System.Drawing.Size(147, 29);
             this.textBox_Shortcut.TabIndex = 3;
@@ -82,6 +90,7 @@ namespace SnippedGenerator
             this.textBox_Description.Location = new System.Drawing.Point(41, 144);
             this.textBox_Description.Multiline = true;
             this.textBox_Description.Name = "textBox_Description";
+            this.textBox_Description.PlaceholderText = "A snipped for Console.ReadLine()";
             this.textBox_Description.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox_Description.Size = new System.Drawing.Size(369, 77);
             this.textBox_Description.TabIndex = 4;
@@ -93,15 +102,95 @@ namespace SnippedGenerator
             this.textBox1.Location = new System.Drawing.Point(41, 272);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Console.ReadLine();";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(740, 299);
+            this.textBox1.Size = new System.Drawing.Size(1003, 239);
             this.textBox1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(549, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2, 264);
+            this.panel1.TabIndex = 6;
+            // 
+            // AddType_Button
+            // 
+            this.AddType_Button.Location = new System.Drawing.Point(569, 75);
+            this.AddType_Button.Name = "AddType_Button";
+            this.AddType_Button.Size = new System.Drawing.Size(121, 33);
+            this.AddType_Button.TabIndex = 7;
+            this.AddType_Button.Text = "Add type";
+            this.AddType_Button.UseVisualStyleBackColor = true;
+            this.AddType_Button.Click += new System.EventHandler(this.AddType_Button_Click);
+            // 
+            // comboBox_types
+            // 
+            this.comboBox_types.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_types.FormattingEnabled = true;
+            this.comboBox_types.Location = new System.Drawing.Point(569, 115);
+            this.comboBox_types.Name = "comboBox_types";
+            this.comboBox_types.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_types.TabIndex = 8;
+            this.comboBox_types.SelectedIndexChanged += new System.EventHandler(this.comboBox_types_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(758, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ToolTip";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(758, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Default value";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox_typeToolTip
+            // 
+            this.textBox_typeToolTip.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_typeToolTip.Location = new System.Drawing.Point(758, 180);
+            this.textBox_typeToolTip.Multiline = true;
+            this.textBox_typeToolTip.Name = "textBox_typeToolTip";
+            this.textBox_typeToolTip.PlaceholderText = "Any variable";
+            this.textBox_typeToolTip.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_typeToolTip.Size = new System.Drawing.Size(228, 58);
+            this.textBox_typeToolTip.TabIndex = 11;
+            this.textBox_typeToolTip.TextChanged += new System.EventHandler(this.textBox_typeToolTip_TextChanged);
+            // 
+            // textBox_defaultValue
+            // 
+            this.textBox_defaultValue.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_defaultValue.Location = new System.Drawing.Point(758, 103);
+            this.textBox_defaultValue.Name = "textBox_defaultValue";
+            this.textBox_defaultValue.PlaceholderText = "var";
+            this.textBox_defaultValue.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox_defaultValue.Size = new System.Drawing.Size(147, 29);
+            this.textBox_defaultValue.TabIndex = 12;
+            this.textBox_defaultValue.TextChanged += new System.EventHandler(this.textBox_defaultValue_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1042, 694);
+            this.ClientSize = new System.Drawing.Size(1221, 726);
+            this.Controls.Add(this.textBox_defaultValue);
+            this.Controls.Add(this.textBox_typeToolTip);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox_types);
+            this.Controls.Add(this.AddType_Button);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox_Description);
             this.Controls.Add(this.textBox_Shortcut);
@@ -124,6 +213,13 @@ namespace SnippedGenerator
         private System.Windows.Forms.TextBox textBox_Shortcut;
         private System.Windows.Forms.TextBox textBox_Description;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button AddType_Button;
+        private System.Windows.Forms.ComboBox comboBox_types;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_typeToolTip;
+        private System.Windows.Forms.TextBox textBox_defaultValue;
     }
 }
 
--- SnippedGenerator/Form1.cs
+++ SnippedGenerator/Form1.cs
@@ -18,14 +18,85 @@ namespace SnippedGenerator
             InitializeComponent();
         }
 
+        // um die TypeUI zu kontrollieren, wenn es beispielsweiße noch keine types gibt, sollen auch noch keine textfelder angezeigt werden
+        public Control[] TypeUI;
+        public List<Types> types;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            TypeUI = new Control[5]; // 5 bc 5 objects
+            types = new List<Types>();
+
+
+            TypeUI[0] = comboBox_types;
+            TypeUI[1] = textBox_defaultValue;
+            TypeUI[2] = label5;
+            TypeUI[3] = label4;
+            TypeUI[4] = textBox_typeToolTip;
+
+            EnableTypeUI(false);
 
         }
+
+        public void EnableTypeUI(bool state)
+        {
+            foreach (var item in TypeUI)
+            {
+                item.Enabled = state;
+            }
+        }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public int currentLastItem = 0;
+        public int currentSelectedItem = 0;
+        private void AddType_Button_Click(object sender, EventArgs e)
+        {
+
+            currentLastItem++;
+            types.Add(new Types());
+            types.Last().Name = $"$type{currentLastItem}$"; 
+            comboBox_types.Items.Add(types.Last().Name);
+            currentSelectedItem = currentLastItem;
+
+            EnableTypeUI(true);
+        }
+
+        private void textBox_defaultValue_TextChanged(object sender, EventArgs e)
+        {
+            types[currentSelectedItem].DefaultValue = textBox_defaultValue.Text;
+        }
+
+        private void textBox_typeToolTip_TextChanged(object sender, EventArgs e)
+        {
+            types[currentSelectedItem].Tooltip = textBox_typeToolTip.Text;
+
+        }
+
+        private void comboBox_types_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+
+    public class Types
+    {
+  
+        public string Name { get; set; }
+        public string DefaultValue { get; set; }
+        public string Tooltip { get; set; }
+
+
+
+    }
+
 }
